d0cc4e7229b02b9c949f1c3f74cbe2705aa3aa71
tayyabadrees <39157259+tayyabadrees@users.noreply.github.com>
2020-01-06 11:59:38 +0500

    Add project files.

---
+++ web Api Project/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace web_Api_Project.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ web Api Project/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="web_Api_Project.WebApiApplication" Language="C#" %>
